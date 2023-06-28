9a5b15de7fb98c0a8b684527088259f40586c9fc
Shyamsaibethina <shyamsaibethina@gmail.com>
2023-06-28 00:12:43 -0400

    "added some dialog"

--- gt_food/lib/food_card.dart
+++ gt_food/lib/food_card.dart
@@ -17,32 +17,48 @@ class FoodCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Color(0xFFFCD0A1),
-        borderRadius: BorderRadius.all(Radius.circular(15)),
-      ),
-      width: 150,
-      margin: const EdgeInsets.fromLTRB(2, 5, 2, 5),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Text(
-              menuItems[hallLocation]![index].food!.name,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w900,
-                  color: Colors.white),
+    return GestureDetector(
+        onTap: () {
+          showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                    content: Text("More info food"),
+                    actions: [
+                      TextButton(
+                        child: Text("OK"),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      )
+                    ],
+                  ));
+        },
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Color(0xFFFCD0A1),
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+          ),
+          width: 150,
+          margin: const EdgeInsets.fromLTRB(2, 5, 2, 5),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text(
+                  menuItems[hallLocation]![index].food!.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: menuIcons[menuItems[hallLocation]![index].id]!,
+                )
+              ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: menuIcons[menuItems[hallLocation]![index].id]!,
-            )
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
